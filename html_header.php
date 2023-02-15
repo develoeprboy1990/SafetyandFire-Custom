22ab48b2092bbea0ac671e54684da8fa23ac28b8
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-02-15 11:46:23 +0500

    Code is indented for readability purpose

--- html_header.php
+++ html_header.php
@@ -1,51 +1,38 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class='no-js' lang='en'>
-<!--<![endif]-->
-<head>
-<meta charset="utf-8" />
-<meta name="description" content="LP Air Conditioning are specialists in the installation, servicing and maintenance of air conditioning systems within commercial and residential premises">
-<meta name="keywords" content="Heating, Ventilating , Air Conditioning Service">
+    <html class='no-js' lang='en'>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="Description" content="Safety and Fire. We are an SABS approved fire equipment  wholesale distributor and servicing specialist of fire extinguishers, fire hose reels, fire hydrants, fire hoses and fire fighting accessories, the refilling of carbon dioxide extinguishers and hydrostatic testing of cylinders.">
+        <meta name="keywords" content="Safety and Fire, fire safety equipment, extinguishers, hose reels, couplings, nozzles, fire blankets, hydrants, valves, Witbank, SABS Certification, Emalahleni, Mpumalanga, Witbank">
+        <meta name="author" content="Safety and Fire, Safty Store">
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no">
+        
+        <title><?php echo $MetaTitle; ?></title>
 
-   <meta name="Description"
-          content="Safety and Fire. We are an SABS approved fire equipment  wholesale distributor and servicing specialist of fire extinguishers, fire hose reels, fire hydrants, fire hoses and fire fighting accessories, the refilling of carbon dioxide extinguishers and hydrostatic testing of cylinders.">
-    <meta name="keywords"
-          content="Safety and Fire, fire safety equipment, extinguishers, hose reels, couplings, nozzles, fire blankets, hydrants, valves, Witbank, SABS Certification, Emalahleni, Mpumalanga, Witbank">
-<meta name="author" content="Safety and Fire Safty Store">
-<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no">
-<!--<title><?#php echo $MetaTitle ?></title>-->
-<title><?php echo $MetaTitle; ?></title>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:700italic,400,700,800,600' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>skdslider.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>product-box.css" />
+        <link rel="stylesheet" media="(min-width: 768px)" type="text/css" href="<?php echo CSS_PATH; ?>nav.css?v=1" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>lightbox.css?v=<?php echo VERSION; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>animate.css?v=<?php echo VERSION; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>selectordie.css?v=<?php echo VERSION; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>responsive-table.css?v=<?php echo VERSION; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>jquery.datetimepicker.css?v=<?php echo VERSION; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>style.css?v=<?php echo VERSION; ?>2" />
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:700italic,400,700,800,600' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>skdslider.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>product-box.css" />
-<link rel="stylesheet" media="(min-width: 768px)" type="text/css" href="<?php echo CSS_PATH; ?>nav.css?v=1" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>lightbox.css?v=<?php echo VERSION; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>animate.css?v=<?php echo VERSION; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>selectordie.css?v=<?php echo VERSION; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>responsive-table.css?v=<?php echo VERSION; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>jquery.datetimepicker.css?v=<?php echo VERSION; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>style.css?v=<?php echo VERSION; ?>2" />
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-<!--[if lt IE 9]>
-<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js"></script>
-<![endif]-->
-<script src="<?php echo JS_PATH; ?>jquery.js?v=<?php echo VERSION; ?>"></script>
-<script src="<?php echo JS_PATH; ?>fbconnect.js?v=<?php echo VERSION; ?>"></script>
-<link rel="icon" type="image/png" href="<?php echo WEB_URL; ?>images/favico.png" />
+        <script src="<?php echo JS_PATH; ?>jquery.js?v=<?php echo VERSION; ?>"></script>
+        <script src="<?php echo JS_PATH; ?>fbconnect.js?v=<?php echo VERSION; ?>"></script>
+        <link rel="icon" type="image/png" href="<?php echo WEB_URL; ?>images/favico.png" />
 
-
-    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
-    <script src="<?php echo JS_PATH; ?>src/jquery.exzoom.js"></script>
-    <link href="<?php echo JS_PATH; ?>src/jquery.exzoom.css" rel="stylesheet" type="text/css"/>
-    <style>
-    #exzoom {
-        width: 400px;
-        /*height: 400px;*/
-    }
-</style>
-</head>
+        <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+        <script src="<?php echo JS_PATH; ?>src/jquery.exzoom.js"></script>
+        <link href="<?php echo JS_PATH; ?>src/jquery.exzoom.css" rel="stylesheet" type="text/css"/>
+        <style>
+            #exzoom {
+                width: 400px;
+                /*height: 400px;*/
+            }
+        </style>
+    </head>
 <body>
