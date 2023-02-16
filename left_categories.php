fd30b1efc51baa066df25591fdaec8c93968c8b6
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-02-16 21:13:55 +0500

    Converted dyanamic php to static markup for left-menu

--- left_categories.php
+++ left_categories.php
@@ -1,4 +1,4 @@
-<aside class="fleft categories-list">
+<!-- <aside class="fleft categories-list">
 	<div class="heading" id="ShowMobileCat"><h2>Browse by Category</h2></div>
     <div class="categories-ul">
     <ul id="menu">
@@ -11,5 +11,38 @@
 		}
 	?>    
     </ul>
+    </div>
+</aside> -->
+<aside class="fleft categories-list">
+	<div class="heading" id="ShowMobileCat"><h2>Browse by Category</h2></div>
+    <div class="categories-ul">
+		<ul id="menu">
+			<li>
+				<a href="http://localhost/ahsan/safetyandfire/productss/category/extinguishers.html">Extinguishers</a>
+				<ul>
+					<li>
+						<a class="tooltip" title="DCP Extinguishers" href="http://localhost/ahsan/safetyandfire/productss/sub-category/dcp-extinguishers.html">DCP Extinguishers</a>
+					</li>
+					<li>
+						<a class="tooltip" title="Co2 Extinguishers" href="http://localhost/ahsan/safetyandfire/productss/sub-category/co2-extinguishers.html">Co2 Extinguishers</a>
+					</li>
+					<li>
+						<a class="tooltip" title="Mobile Extinguishers" href="http://localhost/ahsan/safetyandfire/productss/sub-category/mobile-extinguishers.html">Mobile Extinguishers</a>
+					</li>
+					<li>
+						<a class="tooltip" title="Co2 Extinguisher Spares & Accessories" href="http://localhost/ahsan/safetyandfire/productss/sub-category/co2-extinguisher-spares-accessories.html">Co2 Extinguisher Spares &...</a>
+					</li>
+					<li>
+						<a class="tooltip" title="DCP Extinguisher Spares & Accessories" href="http://localhost/ahsan/safetyandfire/productss/sub-category/dcp-extinguisher-spares-accessories.html">DCP Extinguisher Spares &...</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="http://localhost/ahsan/safetyandfire/productss/category/hose-reels.html">Hose Reels</a><ul><li><a class="tooltip" title="Fire Hose Reels " href="http://localhost/ahsan/safetyandfire/productss/sub-category/fire-hose-reels.html">Fire Hose Reels </a><li><a class="tooltip" title="Fire Hose Reel Spares" href="http://localhost/ahsan/safetyandfire/productss/sub-category/fire-hose-reel-spares.html">Fire Hose Reel Spares</a></ul>
+			</li>
+			<li><a href="http://localhost/ahsan/safetyandfire/productss/category/hydrants.html">Hydrants</a><ul><li><a class="tooltip" title="Hydrants and Assessories" href="http://localhost/ahsan/safetyandfire/productss/sub-category/hydrants-and-assessories.html">Hydrants and Assessories</a><li><a class="tooltip" title="Layflats Hose,Nozzles and Couplings" href="http://localhost/ahsan/safetyandfire/productss/sub-category/layflats-hosenozzles-and-couplings.html">Layflats Hose,Nozzles and...</a></ul></li>
+			<li><a href="http://localhost/ahsan/safetyandfire/productss/category/brackets-and-cabinets.html">Brackets and Cabinets</a><ul><li><a class="tooltip" title="Brackets" href="http://localhost/ahsan/safetyandfire/productss/sub-category/brackets.html">Brackets</a><li><a class="tooltip" title="Cabinets and Covers" href="http://localhost/ahsan/safetyandfire/productss/sub-category/cabinets-and-covers.html">Cabinets and Covers</a></ul></li>
+			<li><a href="http://localhost/ahsan/safetyandfire/productss/category/general-accessories.html">General Accessories</a><ul><li><a class="tooltip" title="Fire Blankets" href="http://localhost/ahsan/safetyandfire/productss/sub-category/fire-blankets.html">Fire Blankets</a><li><a class="tooltip" title="Ring Gauges and General Accessories" href="http://localhost/ahsan/safetyandfire/productss/sub-category/ring-gauges-and-general-accessories.html">Ring Gauges and General A...</a><li><a class="tooltip" title="Alarm Station" href="http://localhost/ahsan/safetyandfire/productss/sub-category/alarm-station.html">Alarm Station</a><li><a class="tooltip" title="Dry Chemical Powder" href="http://localhost/ahsan/safetyandfire/productss/sub-category/dry-chemical-powder.html">Dry Chemical Powder</a><li><a class="tooltip" title="Signs And Labels" href="http://localhost/ahsan/safetyandfire/productss/sub-category/signs-and-labels.html">Signs And Labels</a></ul></li>   
+		</ul>
     </div>
 </aside>
