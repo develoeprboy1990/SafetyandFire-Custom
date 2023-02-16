4078f98ea1e65654dcfb476a4f854e526541d2c0
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-02-16 21:15:09 +0500

    JavaScript file added to keep main menu JS

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+// Navbar Script
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
